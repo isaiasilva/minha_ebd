95701d621ff7ea4a95fd077064866a337934ac1f
Cláudio Oliveira <claudiooliveirajr1994@outlook.com>
2021-12-28 22:48:26 -0300

    Retirando o botão de editar em Aluno por Turma

--- resources/views/user/aluno-por-turma.blade.php
+++ resources/views/user/aluno-por-turma.blade.php
@@ -23,14 +23,11 @@
                     <td>{{ $turma->find($aluno->turma_id)->nome_turma }}</td>
                     @if(Auth::user()->perfil_id === "1")
                         <td>
-                                <span class="d-flex justify-content-around">
-                                    <button class="btn btn-primary"  alt="Editar"><i class="fas fa-edit"></i></button>
                                     <form action="{{route('excluir-turma')}}" method="post" onsubmit="return confirm('Tem certeza?')" >
                                         @csrf
                                         <input type="hidden" name="turma_id" value="{{ $turma->id }}">
                                         <button class="btn btn-danger" alt="Excluir"  ><i class="fa fa-eraser" aria-hidden="true"></i></button>
                                     </form>
-                                </span>
                         </td>
                     @endif
                 </tr>
