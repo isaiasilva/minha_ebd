ea10da073f39a5bf7ea25d95ace7ce766c95f4d4
Cláudio Oliveira <claudiooliveirajr1994@outlook.com>
2022-01-04 21:05:56 -0300

    Ajustanto CSS do altera turma

--- resources/views/user/chamada.blade.php
+++ resources/views/user/chamada.blade.php
@@ -6,18 +6,19 @@
 
 @if(Auth::user()->perfil_id !== "2")
 @section('botao')
+
     <div class="btn-group">
-        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            Alterar turma
+        <button type="button" class="btn btn-info  dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-exchange-alt"></i> Alterar
         </button>
         @if(Auth::user()->perfil_id === "1")
-            <div class="dropdown-menu">
+            <div class="dropdown-menu altera-curso">
                 @foreach($turmas->all() as $turma)
                     <a class="dropdown-item" href="/user/chamada/{{$turma->id}}">{{ $turma->nome_turma}}</a>
                 @endforeach
             </div>
         @else
-            <div class="dropdown-menu">
+            <div class="dropdown-menu altera-curso">
                 @foreach($minhasTurmas as $minhaTurma)
                     <a class="dropdown-item" href="/user/chamada/{{$minhaTurma->turma_id}}">{{$turmas->find($minhaTurma->turma_id)->nome_turma}}</a>
                 @endforeach
