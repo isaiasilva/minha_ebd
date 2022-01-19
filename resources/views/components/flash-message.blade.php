b24c0002a087f864830781ecdc290501a8555216
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-01-19 12:29:20 -0300

    Atualização das flash masage

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,7 +1,7 @@
 @if ($message = Session::get('success'))
     <div class="alert alert-success alert-dismissible fade show">
         <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+        <strong><i class="fas fa-check-circle"></i> {{ $message }}</strong>
     </div>
 @endif
 
@@ -9,7 +9,7 @@
 @if ($message = Session::get('error'))
     <div class="alert alert-danger alert-dismissible fade show">
         <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+        <strong><i class="fas fa-exclamation-circle"></i> {{ $message }}</strong>
     </div>
 @endif
 
@@ -17,7 +17,7 @@
 @if ($message = Session::get('warning'))
     <div class="alert alert-warning alert-dismissible fade show">
         <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+        <strong><i class="fas fa-exclamation-triangle"></i> {{ $message }}</strong>
     </div>
 @endif
 
@@ -25,7 +25,7 @@
 @if ($message = Session::get('info'))
     <div class="alert alert-info alert-dismissible fade show">
         <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $message }}</strong>
+        <strong><i class="fas fa-info-circle"></i> {{ $message }}</strong>
     </div>
 @endif
 
