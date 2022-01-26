8839371a1d2251660a67753b1dc46dcfd5fd8207
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-01-25 21:47:23 -0300

    Refatorando Associação de aluno por turma

--- resources/views/user/novo-aluno-por-turma.blade.php
+++ resources/views/user/novo-aluno-por-turma.blade.php
@@ -5,46 +5,41 @@
 @endsection
 @section('conteudo')
     @include('components.flash-message')
-    <form method="post">
-        @csrf
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="aluno">Aluno</label>
-                        <select class="form-control" name="aluno" aria-label="Default select example" required>
-                            <option selected value="">Selecione</option>
-                            @foreach($alunos as $aluno)
-                                <option value="{{$aluno->id}}">{{$aluno->name}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="turma">Turma</label>
+    <div class="table-responsive">
+        <table class="table" id="datatablesSimple">
+            <thead>
+            <tr class="">
+                <th scope="col">Aluno</th>
+                <th scope="col">Turma</th>
+                <th scope="col">Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+              @foreach($alunos as $aluno)
+                <tr>
+                    <form  method="post" >
+                        @csrf
+                        <input type="hidden" name="aluno" value="{{ $aluno->id }}">
+                        <td>{{ $aluno->name }}</td>
+                    <td>
                         <select class="form-control" name="turma" aria-label="Default select example" required>
                             <option selected value="">Selecione</option>
                             @foreach($turmas as $turma)
                                 <option value="{{$turma->id}}">{{$turma->nome_turma}}</option>
                             @endforeach
                         </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col">
-                    <button type="submit" class="btn btn-primary mt-3">
-                        Associar
-                    </button>
-                </div>
-            </div>
-        </div><!-- FIM DO CONTAINER -->
-    </form>
+                    </td>
+                        <td>
+                            <button type="submit" class="btn btn-primary">
+                                Associar
+                            </button>
+                        </td>
+                    </form>
+                </tr>
+              @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 
