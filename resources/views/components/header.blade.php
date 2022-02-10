517c40b184ba00c9512da82684657b567273942b
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-02-10 17:06:42 -0300

    Melhoria no SEO

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
+    <meta name="keywords" content="Escola Bíblica Dominical, EBD, Chamada, Gameficação, Alunos"/>
+    <meta name="robots" content="index,follow">
+    <meta name="author" content="Chamada EBD - Cláudio Oliveira">
+    <meta name="theme-color" content="#4267b2">
+    <link rel="icon" href="{{asset('img/logo.png')}}">
+    <meta property="og:title" content="Chamada EBD">
+    <meta property="og:site_name" content="Chamada EBD">
+    <meta property="og:description" content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
+    <meta property="og:url" content="https://chamadaebd.com.br/">
+    <meta property="og:image" content="https://chamadaebd.com.br/img/logo.png">
+    <meta property="og:image:type" content="image/png">
     <title>{{$title}}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
