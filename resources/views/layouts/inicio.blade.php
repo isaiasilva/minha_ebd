d8149fbfc39a4798094cbe9e6b232d98b8072739
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-02-10 17:25:25 -0300

    Ajustando favicon

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -7,12 +7,12 @@
     <meta name="keywords" content="Escola Bíblica Dominical, EBD, Chamada, Gameficação, Alunos"/>
     <meta name="robots" content="index,follow">
     <meta name="author" content="Chamada EBD - Cláudio Oliveira">
-    <link rel="icon" href="{{asset('img/logo.png')}}">
+    <link rel="icon" href="{{asset('img/logo_sem_fundo.png')}}">
     <meta property="og:title" content="Chamada EBD">
     <meta property="og:site_name" content="Chamada EBD">
     <meta property="og:description" content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
     <meta property="og:url" content="https://chamadaebd.com.br/">
-    <meta property="og:image" content="https://chamadaebd.com.br/img/logo.png">
+    <meta property="og:image" content="https://chamadaebd.com.br/img/logo_sem_fundo.png">
     <meta property="og:image:type" content="image/png">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Chamada EBD</title>
