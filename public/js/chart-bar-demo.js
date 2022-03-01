b5c113737b2abe2b7b7f9f96dd58daff621ea391
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-03-01 08:07:15 -0300

    Aumentando número de chamadas no gráfico

--- public/js/chart-bar-demo.js
+++ public/js/chart-bar-demo.js
@@ -37,7 +37,7 @@ var myLineChart = new Chart(ctx, {
             yAxes: [{
                 ticks: {
                     min: 0,
-                    max: 200,
+                    max: 300,
                     maxTicksLimit: 5
                 },
                 gridLines: {
