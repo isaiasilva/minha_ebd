a7803e3a358b14b39353e6644004e9a87a07c85e
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2022-04-03 14:54:03 -0300

    Inserindo Livewire

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,6 +15,7 @@
     <title>{{$title}}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    @livewireStyles
     <link rel="stylesheet" href="{{asset('css/all.css')}}">
     <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
     <link rel="stylesheet" href="{{asset('css/dataTables.css')}}">
