692deb92f8605bb4710a50d4f21f30d3635be77e
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-02-04 23:33:23 -0300

    Removendo title do header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
-    <meta name="keywords" content="Escola Bíblica Dominical, EBD, Chamada, Gameficação, Alunos"/>
+    <meta name="description"
+        content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
+    <meta name="keywords" content="Escola Bíblica Dominical, EBD, Chamada, Gameficação, Alunos" />
     <meta name="robots" content="index,follow">
     <meta name="author" content="Chamada EBD - Cláudio Oliveira">
-    <link rel="icon" href="{{asset('img/logo.png')}}">
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
     <meta property="og:title" content="Chamada EBD">
     <meta property="og:site_name" content="Chamada EBD">
-    <meta property="og:description" content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
+    <meta property="og:description"
+        content="Chamada EBD - Sistema de cadastro de alunos e registro de chamadas para Escola Bíblica Dominical.">
     <meta property="og:url" content="https://chamadaebd.com.br/">
     <meta property="og:image" content="https://chamadaebd.com.br/img/logo.png">
     <meta property="og:image:type" content="image/png">
-    <title>{{$title}}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <title>Chamado EBD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
     @livewireStyles
-    <link rel="stylesheet" href="{{asset('css/all.css')}}">
-    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
-    <link rel="stylesheet" href="{{asset('css/dataTables.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dataTables.css') }}">
 </head>
