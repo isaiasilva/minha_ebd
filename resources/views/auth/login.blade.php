dd1ce3e86ea217b2811d793fbd1d52e292a19c3e
Francisco Claudio de Oliveira Junior <claudiooliveira@aridesa.com.br>
2023-03-01 15:23:30 -0300

    Inserindo valor old na view de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.inicio')
 
 @section('conteudo')
-
     <form method="post" action="/entrar">
         @csrf
         <div class="container-fluid">
@@ -12,20 +11,24 @@
                             <img src="{{ asset('img/logo_sem_fundo.png') }}" class="img-fluid pt-5" alt="login">
                             @include('components.flash-message')
                             <p class="lead mb-4">Faça o login para continuar</p>
-                            <input class="input border-0 border-bottom p-2" placeholder="E-mail" type="email" name="email" required/>
+                            <input class="input border-0 border-bottom p-2" placeholder="E-mail" type="email"
+                                name="email" value="{{ old('email') }}" required />
                             <br />
-                            <input class="input border-0 border-bottom p-2" placeholder="Senha" type="password" name="password" required />
+                            <input class="input border-0 border-bottom p-2" placeholder="Senha" type="password"
+                                name="password" required />
                             <br />
                             <div class="mt-4 d-flex justify-content-between align-items-center">
                                 <button class="btn btn-primary">Logar</button>
                             </div>
 
                             <hr class="m-5" />
-                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+        </script>
     </form>
 @endsection
