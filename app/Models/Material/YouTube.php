8cd9fa78e8dcf989a998c3226ca5fa372cc8ee35
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-05-03 22:26:48 -0300

    Inserindo modelos para cadastro de materiais

---
+++ app/Models/Material/YouTube.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Material;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YouTube extends Model
+{
+    use HasFactory;
+}
