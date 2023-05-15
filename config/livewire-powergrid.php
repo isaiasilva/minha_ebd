b6722a81311eb5f6e08821698201863c832d896e
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-05-15 11:14:19 -0300

    Criando arquivo de configuração para template powergrid usando bootstrap 4

--- config/livewire-powergrid.php
+++ config/livewire-powergrid.php
@@ -12,7 +12,8 @@ return [
     */
 
     //'theme' => \PowerComponents\LivewirePowerGrid\Themes\Tailwind::class,
-    'theme' => \PowerComponents\LivewirePowerGrid\Themes\Bootstrap5::class,
+    //'theme' => \PowerComponents\LivewirePowerGrid\Themes\Bootstrap5::class,
+    'theme' => \App\View\PowerGrid\Bootstrap4::class,
 
     /*
     |--------------------------------------------------------------------------
