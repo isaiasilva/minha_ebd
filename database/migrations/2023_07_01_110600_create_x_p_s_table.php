f4645b52347052895d7705237ecfefc4d197276c
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-07-01 12:18:59 -0300

    Criando modelo de XP

---
+++ database/migrations/2023_07_01_110600_create_x_p_s_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('x_p_s', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('igreja_id')->constrained()->cascadeOnDelete();
+            $table->integer('points')->default(0);
+            $table->string('year')->default(date('Y'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('x_p_s');
+    }
+};
