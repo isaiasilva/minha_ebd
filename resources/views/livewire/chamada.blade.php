a9f82071bbeeaad33a83f9654618f4fe0f1aad79
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-08-08 22:13:26 -0300

    Refatorando estilo do botão da chamada

--- resources/views/livewire/chamada.blade.php
+++ resources/views/livewire/chamada.blade.php
@@ -7,24 +7,23 @@
             @section('botao')
                 <div class="btn-group">
                     <button type="button" class="btn btn-info  dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        <i class="fas fa-exchange-alt"></i> Alterar
+                        <i class="fas fa-exchange-alt"></i>
                     </button>
-                    @if (Auth::user()->perfil_id == App\Models\Perfil::SUPERINTENDENTE ||
-                            Auth::user()->perfil_id == App\Models\Perfil::ADMINISTRADOR)
+                    @can('admin_superintendente')
                         <div class="dropdown-menu altera-curso">
                             @foreach ($turmas->all() as $turma)
-                                <a class="dropdown-item"
-                                    href="/user/chamada/{{ $turma->id }}">{{ $turma->nome_turma }}</a>
+                                <a class="dropdown-item" href="/user/chamada/{{ $turma->id }}">{{ $turma->nome_turma }}</a>
                             @endforeach
                         </div>
-                    @else
+                    @elsecan()
                         <div class="dropdown-menu altera-curso">
                             @foreach ($minhasTurmas as $minhaTurma)
                                 <a class="dropdown-item"
                                     href="/user/chamada/{{ $minhaTurma->id }}">{{ $minhaTurma->nome_turma }}</a>
                             @endforeach
                         </div>
-                    @endif
+                    @endcan
+
                 </div>
             @endsection
         </div>
